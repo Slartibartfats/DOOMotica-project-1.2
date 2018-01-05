b574fe7c7df8657d2fc53c72eef01e4dc327409b
nielstuur <33862371+nielstuur@users.noreply.github.com>
2018-01-05 20:07:02 +0100

    gekloot met login opzet is er

--- VS project/DOOMotica 1.2/DOOMotica 1.2/Login.aspx
+++ VS project/DOOMotica 1.2/DOOMotica 1.2/Login.aspx
@@ -2,4 +2,17 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Label ID="lbl_ConnFeedBack" runat="server"></asp:Label>
+    <br />
+    <asp:Label ID="Label1" runat="server" Text="Username"></asp:Label>
+    <br />
+    <asp:TextBox ID="txt_Username" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Password"></asp:Label>
+    <br />
+    <asp:TextBox ID="txt_Password" runat="server"></asp:TextBox>
+    <br />
+    <asp:Button ID="btn_Login" runat="server" Text="Login" OnClick="btn_Login_Click" />
+
+
 </asp:Content>
--- VS project/DOOMotica 1.2/DOOMotica 1.2/Login.aspx.cs
+++ VS project/DOOMotica 1.2/DOOMotica 1.2/Login.aspx.cs
@@ -4,13 +4,45 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.OleDb;
 
 namespace DOOMotica_1._2
 {
     public partial class Login : System.Web.UI.Page
     {
+        OleDbConnection Conn = new OleDbConnection();
+        Conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Server.MapPath(@"\App_data") + @"\Web-Applicatie Database.accdb";
+
+        OleDbCommand cmd = new OleDbCommand();
+        cmd.Connection = conn;
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void btn_Login_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Conn.Open();
+                OleDbDataReader reader = cmd.ExecuteReader();
+                
+
+
+
+            }
+            catch (Exception exc)
+            {
+                lbl_ConnFeedBack.Text = exc.Message;
+            }
+            finally
+            {
+
+                Conn.Close();
+                
+            }
 
         }
     }
