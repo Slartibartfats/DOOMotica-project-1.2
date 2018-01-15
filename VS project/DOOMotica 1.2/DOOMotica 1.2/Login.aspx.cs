4e2d61176ac55733f3633978142178d13ee4e509
nielstuur <33862371+nielstuur@users.noreply.github.com>
2018-01-15 12:28:11 +0100

    Login opzet alleen nog hashen en vergelijken met database

--- VS project/DOOMotica 1.2/DOOMotica 1.2/Login.aspx.cs
+++ VS project/DOOMotica 1.2/DOOMotica 1.2/Login.aspx.cs
@@ -11,15 +11,16 @@ namespace DOOMotica_1._2
     public partial class Login : System.Web.UI.Page
     {
         OleDbConnection Conn = new OleDbConnection();
-        Conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Server.MapPath(@"\App_data") + @"\Web-Applicatie Database.accdb";
-
         OleDbCommand cmd = new OleDbCommand();
-        cmd.Connection = conn;
+
+        string Username, Password, Wachtwoord, Param1;
+
 
 
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Server.MapPath(@"\App_data") + @"\Web-Applicatie Database.accdb";
+            cmd.Connection = Conn;
         }
 
         protected void btn_Login_Click(object sender, EventArgs e)
@@ -27,9 +28,25 @@ namespace DOOMotica_1._2
             try
             {
                 Conn.Open();
-                OleDbDataReader reader = cmd.ExecuteReader();
-                
+                cmd.CommandText = "SELECT Wachtwoord FROM LID WHERE Gebruikersnaam = ? ";
+                cmd.Parameters.Clear(); //verwijderen eerdere parameters
+                Param1 = Username;
+                cmd.Parameters.Add(Param1);
+                OleDbDataReader Reader = cmd.ExecuteReader();
+                Username = txt_Username.Text;
+                Password = txt_Password.Text;
 
+                Wachtwoord = Reader.ToString();
+
+                if(Password == Wachtwoord)
+                {
+                    //ga naar begin pagina
+                }
+                else
+                {
+                    //foutmelding
+                }
+                
 
 
             }
