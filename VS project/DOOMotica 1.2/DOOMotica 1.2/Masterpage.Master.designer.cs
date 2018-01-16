05926fce9eb8a3065fe1ac76a223899bd408f0d6
Timo van der Steenhoven <522397@student.INHolland.nl>
2018-01-16 13:27:07 +0100

    Salt + HAsh

--- VS project/DOOMotica 1.2/DOOMotica 1.2/Masterpage.Master.designer.cs
+++ VS project/DOOMotica 1.2/DOOMotica 1.2/Masterpage.Master.designer.cs
@@ -40,6 +40,15 @@ namespace DOOMotica_1._2 {
         protected global::System.Web.UI.WebControls.ImageButton mgbtn_Hlogo;
         
         /// <summary>
+        /// Navigatie control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Navigatie;
+        
+        /// <summary>
         /// cntnt_Body control.
         /// </summary>
         /// <remarks>
