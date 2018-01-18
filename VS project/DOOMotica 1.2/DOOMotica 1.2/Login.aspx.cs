14dcbf6bf76978f9436303c66ea5acc4225fa5c7
nielstuur <33862371+nielstuur@users.noreply.github.com>
2018-01-18 14:42:04 +0100

    login verbeterd alleen nog niet af

--- VS project/DOOMotica 1.2/DOOMotica 1.2/Login.aspx.cs
+++ VS project/DOOMotica 1.2/DOOMotica 1.2/Login.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.OleDb;
+using System.Security.Cryptography;
 
 namespace DOOMotica_1._2
 {
@@ -12,6 +13,9 @@ namespace DOOMotica_1._2
     {
         OleDbConnection Conn = new OleDbConnection();
         OleDbCommand cmd = new OleDbCommand();
+        byte[] salt;
+
+        
 
         string Username, Password, Wachtwoord, Param1;
 
@@ -21,20 +25,46 @@ namespace DOOMotica_1._2
         {
             Conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Server.MapPath(@"\App_data") + @"\Web-Applicatie Database.accdb";
             cmd.Connection = Conn;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+            
         }
 
         protected void btn_Login_Click(object sender, EventArgs e)
         {
+
+            Username =txt_Username.Text;
+            cmd.CommandText = "SELECT Wachtwoord FROM LID WHERE Gebruikersnaam = ? ";
+            cmd.Parameters.Clear(); //verwijderen eerdere parameters
+            Param1 = Username;
+            cmd.Parameters.Add(Param1);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(txt_Password.Text, salt, 10000);
+
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+
+
             try
             {
                 Conn.Open();
-                cmd.CommandText = "SELECT Wachtwoord FROM LID WHERE Gebruikersnaam = ? ";
-                cmd.Parameters.Clear(); //verwijderen eerdere parameters
-                Param1 = Username;
-                cmd.Parameters.Add(Param1);
                 OleDbDataReader Reader = cmd.ExecuteReader();
-                Username = txt_Username.Text;
-                Password = txt_Password.Text;
+                
+                if(Reader == null)
+                {
+                    lbl_ConnFeedBack.Text = "Er is een fout opgetreden";
+                }
+                else
+                {
+                    while(Reader.Read())
+                    {
+                        Wachtwoord += string.Format("{0}: {1}<br />\n", Reader["Wachtwoord"].ToString());
+                    }
+                }
 
                 Wachtwoord = Reader.ToString();
 
